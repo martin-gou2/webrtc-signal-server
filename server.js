2dbc42c7bf1bc4eeb3355a1d972798ef0338e608
martin-gou2 <martin.s.strnad@gmail.com>
2025-10-29 11:26:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,15 +4,28 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: { origin: '*' }
-});
+const io = new Server(server, { cors: { origin: '*' } });
 
 io.on('connection', socket => {
   console.log('Uživatel připojen:', socket.id);
 
-  socket.on('signal', data => {
-    io.to(data.to).emit('signal', { from: socket.id, signal: data.signal });
+  socket.on('join', room => {
+    socket.join(room);
+    const clients = io.sockets.adapter.rooms.get(room);
+    const numClients = clients ? clients.size : 0;
+
+    if (numClients === 1) {
+      socket.emit('created', room);
+    } else if (numClients === 2) {
+      io.to(room).emit('ready');
+      socket.emit('joined', room);
+    } else {
+      socket.emit('full', room);
+    }
+  });
+
+  socket.on('signal', ({ room, data }) => {
+    socket.to(room).emit('signal', { data });
   });
 
   socket.on('disconnect', () => {
@@ -21,4 +34,4 @@ io.on('connection', socket => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server běží na portu ${PORT}`));
+server.listen(PORT, () => console.log(`Signální server běží na portu ${PORT}`));
